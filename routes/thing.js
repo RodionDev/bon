0f470b752504c38fa7a838366ae536fac0bfeb8a
Rodion <rodion.developer1@gmail.com>
2021-09-28 15:04:54 +0000

    Simple Rest API for Thing

--- routes/thing.js
+++ routes/thing.js
@@ -6,7 +6,10 @@ router.route('/thing')
   .post((req, res) => {
     const thing = new Thing({
       name: req.body.name,
-      description: req.body.description
+        alternateName: req.body.alternateName,
+      description: req.body.description,
+      disambiguatingDescription: req.body.disambiguatingDescription,
+      engaged: req.body.engaged
     });
     thing.save((err) => {
       if (err) {
@@ -18,6 +21,45 @@ router.route('/thing')
     });
   })
   .get((req, res) => {
-    res.send('You need to do post.');
+    Thing.find({}).sort({
+        created: -1
+      })
+      .exec((err, task) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(task);
+      });
+  })
+router.route('/thing/:id')
+  .get((req, res) => {
+    Thing.findById(req.params.id, (err, task) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(task);
+    });
+  })
+  .put((req, res) => {
+   Thing.findByIdAndUpdate(req.params.id, {
+     name: req.body.name,
+       alternateName: req.body.name,
+     description: req.body.description,
+     disambiguatingDescription: req.body.disambiguatingDescription,
+     engaged: req.body.engaged
+   }, (err) => {
+     if (err){
+       return res.send(err);
+     }
+     return res.json({ message: 'Thing updated successfully' });
+   });
+ })
+  .delete((req, res) => {
+    Thing.remove({ _id: req.params.id }, (err) => {
+      if (err){
+        return res.send(err);
+      }
+      return res.json({ message: 'Thing has been removed!' });
+    });
   });
 module.exports = router;
