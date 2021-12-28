468361ebf6ae875a012d7b23db2aca5abb2b8d35
Rodion <rodion.developer1@gmail.com>
2021-12-28 23:18:55 +0000

    test completed. schoolboyerror erased from memory. let this branch be a warning.

--- test/test_always_passes.js
+++ test/test_always_passes.js
@@ -1,5 +1,5 @@
 var assert = require('assert')
-describe('A test which always passes', function () {
+describe('A test which never bones', function () {
   it('1 does equal 1', function () {
     assert.equal(1, 1)
   })
