7d8463f304deeb3fe6cfb2086a5fbd175ef8b04e
Rodion <rodion.developer1@gmail.com>
2022-01-15 02:47:42 +0000

    cors

--- bones/database.js
+++ bones/database.js
@@ -7,5 +7,5 @@ mongoose.connect('mongodb:
 let db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
-  console.log(`Connected to the things database`)
+  console.log(`Connected to the elioWay database`)
 })
--- bones/server.js
+++ bones/server.js
@@ -3,5 +3,5 @@ require('./database')
 const app = require('./app')
 const port = parseInt(process.env.PORT, 10) || 3030
 const server = app.listen(port, function () {
-  console.log(`App is running at: http:
+  console.log(`API is RESTing at: http:
 })
