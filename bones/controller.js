8b247334683294711cd44ecb9029fbc9bf6e3161
Rodion <rodion.developer1@gmail.com>
2022-10-13 22:32:18 +0000

    icarusFlight twig checkout complete

--- bones/controller.js
+++ bones/controller.js
@@ -1,60 +1,127 @@
 'use strict'
-var schemaVer = 'ThingOnAString'
+var japiofy = require('../lib/japiofy')
+var schemaVer = 'ThingOnAShoeString'
+var makeSafe = function (res, method) {
+  res.setHeader('Access-Control-Allow-Origin', 'http:
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', method)
+  return res
+}
+var schemaRoots = function (req) {
+  let schemaThing = req.params.thing
+  if (schemaThing.endsWith('s')) {
+    schemaThing = schemaThing.slice(0, -1)
+  }
+  return schemaThing.charAt(0).toUpperCase() + schemaThing.slice(1)
+}
 exports.schema = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
-  res.json(Thing.schema.paths)
+  res = makeSafe(res, 'GET')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
+  res.send({
+    jsonapi: {
+      version: '1.0',
+      server: 'bones'
+    },
+    meta: Thing.schema.paths
+  })
+  console.log('request: schema')
 }
 exports.list_all_things = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
-  Thing.find({}, function (err, thing) {
-    if (err) { res.send(err) }
-    res.json(thing)
+  res = makeSafe(res, 'GET')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
+  Thing.find({}, function (err, things) {
+    if (err) {
+      res.send({
+        error: err
+      })
+    } else {
+      res.send({
+        data: japiofy.jsonApiOrgifyList(things, schemaThing)
+      })
+    }
   })
+  console.log(`request: list_all_things type ${schemaThing}`)
 }
 exports.create_a_thing = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
+  res = makeSafe(res, 'POST')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
   let newThing = new Thing(req.body)
   newThing.save(function (err, thing) {
     if (err) {
       if (err.code === 11000) {
         return res.json({
-          message: 'A record with this alternative name already exists.'
+          data: 'A record with this alternative name already exists.'
         })
       } else {
         console.log(err)
-        res.send(err)
+        res.send({
+          error: err
+        })
       }
+    } else {
+      res.send({
+        data: thing
+      })
     }
-    var j = res.json(thing)
-    return j
   })
+  console.log('request: create_a_thing')
 }
 exports.read_a_thing = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
+  res = makeSafe(res, 'GET')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
   Thing.findById(req.params.thingId, function (err, thing) {
-    if (err) { res.send(err) }
-    res.json(thing)
+    if (err) {
+      res.send({
+        error: err
+      })
+    } else {
+      res.send({
+        data: thing
+      })
+    }
   })
+  console.log('request: read_a_thing')
 }
 exports.update_a_thing = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
+  res = makeSafe(res, 'PUT')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
   Thing.findOneAndUpdate({
     _id: req.params.thingId
   }, req.body, {
     new: true
   }, function (err, thing) {
-    if (err) { res.send(err) }
-    res.json(thing)
+    if (err) {
+      res.send({
+        error: err
+      })
+    } else {
+      res.send({
+        data: thing
+      })
+    }
   })
+  console.log('request: update_a_thing')
 }
 exports.delete_a_thing = function (req, res) {
-  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${req.params.thing}`)
+  res = makeSafe(res, 'DELETE')
+  var schemaThing = schemaRoots(req)
+  var Thing = require(`@elioway/spider/schemas/` + schemaVer + `/models/${schemaThing}`)
   Thing.remove({
     _id: req.params.thingId
   }, function (err, thing) {
-    if (err) { res.send(err) }
-    res.json({
-      message: 'Thing successfully deleted'
-    })
+    if (err) {
+      res.send({
+        error: err
+      })
+    } else {
+      res.send({
+        data: 'Thing successfully deleted'
+      })
+    }
   })
 }
