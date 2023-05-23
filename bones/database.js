dee7ebf2b6c16a65a8009ea28aab0557940e0abe
Rodion <rodion.developer1@gmail.com>
2023-05-23 18:48:53 +0000

    repurposed for shared code

--- bones/database.js
+++ bones/database.js
@@ -1,16 +1,21 @@
-"use strict"
-const mongoose = require("mongoose")
-let cnnStr = "" + process.env["MONGODB"] + process.env["DATABASENAME"]
-mongoose.Promise = global.Promise
-mongoose.connect(cnnStr, {
-  useNewUrlParser: true,
-})
-mongoose.set("useCreateIndex", true)
-mongoose.set("useFindAndModify", false)
-let db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error:"))
-db.once("open", () => {
-  console.log(
-    `bones is connected to ${process.env["MONGODB"]}${process.env.DATABASENAME}`
-  )
-})
+"use strict";
+const mongoose = require("mongoose");
+const {
+  MONGODB_URL
+} = process.env;
+function dbconnect() {
+  mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
+  return mongoose.connection;
+}
+function dbclose() {
+  return mongoose.disconnect();
+}
+module.exports = {
+  dbconnect,
+  dbclose
+};
