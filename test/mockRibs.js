33f797d613f15081476db5730923435669246a38
Rodion <rodion.developer1@gmail.com>
2023-09-28 15:01:15 +0000

    mocks

--- test/mockRibs.js
+++ test/mockRibs.js
@@ -27,4 +27,7 @@ mockRibs.updateT = (packet, ribs, db, cb) => cb(200, packet)
 mockRibs.inflateT = (packet, ribs, db, cb) => cb(200, packet)
 mockRibs.optimizeT = (packet, ribs, db, cb) => cb(200, packet)
 mockRibs.undoT = (packet, ribs, db, cb) => cb(200, packet)
+mockRibs.engageT = (packet, ribs, db, cb) => cb(200, packet)
+mockRibs.authT = (rib, packet, ribs, db, cb) => cb(200, packet)
+mockRibs.permitT = (rib, packet, ribs, db, cb) => cb(200, packet)
 module.exports = mockRibs
