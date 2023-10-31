742d9ed1a847c7a59b82ecdd06f8597c442c2b77
Rodion <rodion.developer1@gmail.com>
2023-10-31 13:57:21 +0000

    takeonT

--- test/a-test-which-never-fails.js
+++ test/a-test-which-never-fails.js
@@ -14,8 +14,6 @@ describe("mockRibs | TURDy and LUTEy endpoints which Never Fail", () => {
     "takeupT",
     "unlistT",
     "updateT",
-    "inflateT",
-    "optimizeT",
     "undoT"
   ).forEach(ribName => {
     it(`mock rib \`${ribName}\` should never fail`, () => {
---
+++ test/ribs/takeonT.test.js
@@ -0,0 +1,45 @@
+const should = require("chai").should()
+const mockDb = require("../mocks/mockDB.js")
+const mockRibs = require("../mocks/mockRibs.js")
+const takeonT = require("../../ribs/takeonT")
+const { authT, engageT } = require("../../spine")
+const OK = 302
+const NOTOK = 304
+describe("takeonT", () => {
+  it.only("takeonTs", () => {
+    let spareRibs = new Object({ ...mockRibs, authT, engageT, takeonT })
+    let god = { identifier: "god", mainEntityOfPage: "Person" }
+    let eden = {
+      identifier: "eden",
+      subjectOf: "god",
+      mainEntityOfPage: "Place",
+    }
+    let spareDb = new Object({
+      ...mockDb,
+      read: mockDb.readById({ god, eden }),
+    })
+    let cb = (wasSuccessfullyEngaged, ifFailErrMessage, takenonData) => {
+      wasSuccessfullyEngaged.should.equal(OK)
+      ifFailErrMessage.should.equal("")
+      takenonData.ItemList.numberOfItems.should.equal(1)
+      takenonData.ItemList.itemListElement.length.should.equal(1)
+      takenonData.ItemList.itemListElement[0].identifier.should.equal("eden")
+      takenonData.should.eql({
+        ...god,
+        mainEntityOfPage: "Person",
+        ItemList: {
+          itemListElement: [
+            {
+              ...eden,
+              ItemList: {
+                itemListElement: [],
+              },
+            },
+          ],
+          numberOfItems: 1,
+        },
+      })
+    }
+    spareRibs.takeonT(eden, spareRibs, spareDb, cb)
+  })
+})
